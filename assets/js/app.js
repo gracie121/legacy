66bc8bcaf8b9ccc7daede0854039102a798db4c3
gracie121 <ezeanigracie419@gmail.com>
2018-12-01 19:55:51 +0100

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,53 @@
+function showNextForm() {
+    $("#First-Div").css("display", "none");
+    $("#Second-Div").css("display", "block");
+  };
+  
+  
+  function showNextForm1() {
+    $("#Second-Div").css("display", "none");
+    $("#Third-Div").css("display", "block");
+  
+  };
+  
+  function showNextForm2() {
+    $("#Third-Div").css("display", "none");
+    $("#Fourth-Div").css("display", "block");
+  
+  };
+  
+  function showNextForm3() {
+    $("#Fourth-Div").css("display", "none");
+    $("#Fivth-Div").css("display", "block");
+  
+  };
+  
+  function showNextForm4() {
+    $("#Fivth-Div").css("display", "none");
+    $("#Six-Div").css("display", "block");
+  
+  };
+  
+  function showNextForm5() {
+    $("#Six-Div").css("display", "none");
+    $("#Seven-Div").css("display", "block");
+  
+  };
+  
+  function showNextForm6() {
+    $("#Seven-Div").css("display", "none");
+    $("#Eight-Div").css("display", "block");
+  
+  };
+  
+  function showNextForm7() {
+    $("#Eight-Div").css("display", "none");
+    $("#Nine-Div").css("display", "block");
+  
+  };
+  
+  function showNextForm8() {
+    $("#Nine-Div").css("display", "none");
+    $("#ten").css("display", "block");
+  
+  };
