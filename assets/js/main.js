92de5a7ef687c437a9eea4a6829fadffab9d7e42
gracie121 <ezeanigracie419@gmail.com>
2018-12-06 15:44:32 +0100

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
-(function() {
+(function () {
   'use strict';
-  window.addEventListener('load', function() {
+  window.addEventListener('load', function () {
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.getElementsByClassName('needs-validation');
     // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
+    var validation = Array.prototype.filter.call(forms, function (form) {
+      form.addEventListener('submit', function (event) {
         if (form.checkValidity() === false) {
           event.preventDefault();
           event.stopPropagation();
@@ -14,4 +14,13 @@
       }, false);
     });
   }, false);
+
 })();
+
+
+// var now, year, month, months;
+// now = new Date();
+// months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+// month = now.getMonth();
+// year = now.getFullYear();
+// document.getElementById("date").textContent = months[month] + ' ' + year;
